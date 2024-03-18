72edaab1a5473f516bbec4eb5978127a41cd7485
rladpwns21 <aron11213217@gmail.com>
2024-03-18 14:43:56 +0900

    char 3

--- first/index.js
+++ first/index.js
@@ -1,2 +1,28 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+    constructor(position)
+    {
+        this.position = position;
+    }
+    draw(){
+        c.fillStyle = "red";
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+
+const player = new Sprite({
+    x :0,
+    y :0,
+});
+
+console.log(player);
+
+player.draw;
